2c92b86b4d20bffb6ccb99ef5cb0cd007e4b4b33
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-21 15:06:43 +0700

    jika file tidak ditemukan maka akan ditampilkan Unable to open file

--- ioFile_023/ioFile_023.cpp
+++ ioFile_023/ioFile_023.cpp
@@ -44,4 +44,7 @@ int main() {
 		// tutup file tersebut setelah selesai
 		infile.close();
 	}
+	// jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
