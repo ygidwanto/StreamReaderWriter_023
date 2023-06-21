2e866bff2d9da95f0942f25ddc4848fc6c2d26bd
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-21 14:55:59 +0700

    menambahkan fungsi outfile untuk menulis dan memasukan nilai dari baris

--- ioFile_023/ioFile_023.cpp
+++ ioFile_023/ioFile_023.cpp
@@ -18,5 +18,9 @@ int main() {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' kedalam file
+		outfile << baris << endl;
 	}
 }
