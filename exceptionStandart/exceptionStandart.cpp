2ef921f7c59d6f4272439fd4e9e5224c57b980c9
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-21 15:55:15 +0700

    selesai

--- exceptionStandart/exceptionStandart.cpp
+++ exceptionStandart/exceptionStandart.cpp
@@ -19,7 +19,7 @@ int main()
 		/*akan dieksekusi karena array data hanya memiliki 3 elemen*/
 	}
 	cout << "Baris Program Yang Terakhir" << endl;
-	/*penenda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
 	*/
 	return 0;
 }
