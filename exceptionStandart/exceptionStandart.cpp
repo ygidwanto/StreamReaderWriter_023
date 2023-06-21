90262545ae20630ed03cc854cb309b309bf0e8d0
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-21 15:55:01 +0700

    menggunakan kembali fungsi catch

--- exceptionStandart/exceptionStandart.cpp
+++ exceptionStandart/exceptionStandart.cpp
@@ -13,4 +13,13 @@ int main()
 		cout << data.at(5)<<endl;
 		//memanggil array elemen ke 5
 	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	/*penenda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	*/
+	return 0;
 }
