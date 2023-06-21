004d97f71721057daaa4640301e19cdcbe8d2c34
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-21 15:43:11 +0700

    membuat contoh manual Excption dengan kata kunci Catch

--- manualExcption/manualExcption.cpp
+++ manualExcption/manualExcption.cpp
@@ -8,4 +8,14 @@ int main()
 		throw 0.5; // melaporkan sebuah interger muka
 		cout << "Pengecualian tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi " << endl;
+	}
+	catch (...) {
+		/*jika selain interjer maka block akan dieksekusi*/
+		cout << "default  pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
 }
