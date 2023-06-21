bdce323b19ecdcbe71240ecd2c67e54ecbd7f017
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-21 15:49:57 +0700

    menampilkan obyek yang akan digunakan

---
+++ exceptionStandart/exceptionStandart.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <exception>
+// untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+int main()
+{
+	cout << "Awal Program " << endl;  //penanda 1:...
+	try {
+		array<int, 3> data = { 3,5,7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5)<<endl;
+		//memanggil array elemen ke 5
+	}
+}
